a813fb471ee843b8733cce04a0884901dd343afa
shivatech-3784 <shivaprasadgudipally2003@gmail.com>
2026-01-08 19:26:42 +0530

    app.js modification

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,24 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.send("AI CX Operations dashboard is working");
-})
+app.use("/api/users", userRoutes);
 
-export default app;
+app.get("/", (req, res) => {
+  res.send("AI CX Operations Dashboard is working");
+});
+
+export { app };

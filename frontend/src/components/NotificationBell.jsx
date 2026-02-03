5229e6818ba22a03cb8aa9d405cb21dd8301b7aa
shivatech-3784 <shivaprasadgudipally2003@gmail.com>
2026-02-04 00:26:34 +0530

    name changed

--- frontend/src/components/NotificationBell.jsx
+++ frontend/src/components/NotificationBell.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSocket } from "../context/socketContext";
-import { fetchNotifications, markAsRead } from "../services/notification.api";
+import { fetchNotifications, markAsRead } from "../services/Notification.api";
 
 const NotificationBell = () => {
   const socket = useSocket();
